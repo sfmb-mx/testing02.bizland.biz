baea7478aae0cfc8ec9b7ef37c573d3b7a7317ab
git <smendoza.barrera@gmail.com>
2015-11-10 15:48:45 -0600

    Chicken prices v0.0.1

--- javascripts/libs/equalheights.js
+++
@@ -1,8 +0,0 @@
-// $.fn.setAllToMaxHeight = function(){
-// return this.height( Math.max.apply(this, $.map( this , function(e){ return $(e).height() }) ) );
-// }
-
-$.fn.setAllToMaxHeight = function(){
-return this.css({ 'height' : '' }).height( Math.max.apply(this, jQuery.map( this , function(e){ return jQuery(e).height() }) ) );
-}
-
